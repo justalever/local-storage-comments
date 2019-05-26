63b5407599150b084bca1b6fc456ff306b78353b
Andy Leverenz <andy@dribbble.com>
2019-05-26 11:07:25 -0500

    Init

---
+++ app.js
@@ -0,0 +1,48 @@
+const submit = document.querySelector('.comment-submit');
+const form = document.querySelector('.comment-form');
+const commentList = document.querySelector('.comments');
+const commentInput = document.querySelector('.comment-input');
+
+function template(data) {
+  commentList.insertAdjacentHTML("beforeend", `
+  <div class="comment flex items-start justify-start">
+      <img class="comment-avatar" src="${data.avatar}" />
+      <div class="flex-1">
+        <h3 class="comment-author">${data.author}</h3>
+        <p class="comment-body">${data.comment}</p>
+      </div>
+    </div>
+  </div>`);
+}
+
+function appendComment (event) {
+
+  const data = {
+    avatar: "https://secure.gravatar.com/avatar/d1f5ca0d7e625f334c5186e112b77ebd",
+    comment: commentInput.value,
+    author: "Andy Leverenz"
+  };
+
+  event.preventDefault();
+  // If the value is nothing just return
+  if (commentInput.value.length < 1) return;
+
+  // Insert new template into DOM
+  template(data);
+
+  // Reset textrea value
+  commentInput.value = "";
+
+  // Save the list to localStorage
+  localStorage.setItem('commentListing', commentList.innerHTML);
+}
+
+submit.addEventListener('click', appendComment, false)
+
+// Check for saved wishlist items
+const saved = localStorage.getItem('commentListing');
+
+// If there are any saved items, update our list
+if (saved) {
+	commentList.innerHTML = saved;
+}
